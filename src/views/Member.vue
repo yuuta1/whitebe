25aa8bcbb0d6b9e21ae5d10a9b55d099236d1df9
yuuta1 <oozoranohonoo@yahoo.co.jp>
2020-08-11 21:27:32 +0900

    first commit

--- src/views/Member.vue
+++ src/views/Member.vue
@@ -1,30 +1,55 @@
 <template>
     <div id="member">
-        <p class="members">会員登録</p>
-        <p><input type="text" placeholder="ユーザー名"></p>
-        <p><input type="email" placeholder="メールアドレス"></p>
-        <p><input type="password" placeholder="password"></p>
+        <div class="members-form">
+            <p class="members">会員登録</p>
+            <p><input type="text" placeholder="ユーザー名"></p>
+            <p><input type="email" placeholder="メールアドレス"></p>
+            <p><input type="password" placeholder="password"></p>
+        </div>
+        <div class=post>
+            <input type="text" v-model="postCode">
+            <button @click="search">表示</button>
+            <p>都道府県:{{address.pref}}</p>
+            <p>市区町村:{{address.city}}</p>
+        </div>
         <a href="#" class="burron">会員登録</a>
-    </div>
+        </div>
 </template>
 
 <script>
-let url ='http://zipcloud.ibsnet.co.jp/api/search?zipcode=';
+let url ='https://apis.postcode-jp.com/api/v3/postcodes?apikey=YhRRFH99ve7rXQyfeksO5B2EaeiFv2WSizdvgth&postcode=';
 const axios = require('axios');
 export default {
-    async created(){
-        let item = await axios.get(
-            url + '721-0902'
-        );
-        console.log(item);
+    data(){
+        return{
+            postCode:'',
+            address:{
+                pref:'',
+                city:'',
+            }
+        }
+    },
+    methods:{
+        async search(){
+            let item = await axios.get(
+                url + this.postCode
+            )
+            const itemPref = item.data.data[0].pref
+            const itemCity = item.data.data[0].city
+            this.address.pref=itemPref
+            this.address.city=itemCity
+            console.log(itemPref)
+            console.log(itemCity)
+        }
     }
 }
 </script>
 <style scoped>
 #member{
-    height:200px;
+    height:300px;
     text-align:center;
 }
+
 
 .members{
     font-size:30px;
